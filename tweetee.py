19b14b9d5efa97115855f2f1da773d7b2d0fdfa8
Prasanthsrinivas <122075964+Prasanthsrinivas@users.noreply.github.com>
2023-01-30 12:35:17 +0530

    Update tweetee.py

--- tweetee.py
+++ tweetee.py
@@ -14,3 +14,15 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
 df = pd.DataFrame(tweets,columns=["Date", "Tweetid","tweet url", "username", "location","content","source", "retweetcount",  "likecount ","replycount","language"])
 df
 
+a_dict=a.to_dict("records")
+print(a_dict)
+
+
+from pymongo import MongoClient
+
+py=MongoClient("mongodb://Guvi:guvidw34@ac-wadx12y-shard-00-00.fr2x1n6.mongodb.net:27017,ac-wadx12y-shard-00-01.fr2x1n6.mongodb.net:27017,ac-wadx12y-shard-00-02.fr2x1n6.mongodb.net:27017/?ssl=true&replicaSet=atlas-jltwa6-shard-0&authSource=admin&retryWrites=true&w=majority")
+pytweet=py["twitterscrapping"]
+pycollection=pytweet["tweets"]
+pycollection.insert_many(a_dict)
+
+
